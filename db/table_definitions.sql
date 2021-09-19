cc06a23013f244ad3e2b6a9836c39d294fa266b4
ryhoh <axis38akasira@gmail.com>
2021-09-20 00:02:06 +0900

    Update table_definitions.sql

--- db/table_definitions.sql
+++ db/table_definitions.sql
@@ -6,21 +6,14 @@ CREATE TABLE public.devices (
 	device_id SERIAL NOT NULL,
 	device_name VARCHAR(32) NOT NULL,
 	last_heartbeat TIMESTAMP(0) NULL,
-	return_message VARCHAR(4096) NOT NULL,
-	has_gpu bool NOT NULL,
+	report VARCHAR(4096) DEFAULT '' NOT NULL,
+	return_message VARCHAR(4096) DEFAULT '' NOT NULL,
 	CONSTRAINT devices_pk PRIMARY KEY (device_id),
 	CONSTRAINT devices_un UNIQUE (device_name)
 );
 
-CREATE TABLE public.gpu_machines (
-	machine_id SERIAL NOT NULL,
-	last_detail VARCHAR(4096) NOT NULL,
-	CONSTRAINT gpu_machines_pk PRIMARY KEY (machine_id),
-	CONSTRAINT gpu_machines_fk FOREIGN KEY (machine_id) REFERENCES public.devices(device_id) ON DELETE RESTRICT ON UPDATE RESTRICT
-);
-
 CREATE TABLE public.jwt (
-	secret VARCHAR(255) NOT NULL
+	secret CHAR(64) NOT NULL
 );
 
 CREATE TABLE public.users (
@@ -34,18 +27,14 @@ CREATE TABLE public.users (
 -- Data Insertion
 --------------------
 
-INSERT INTO public.devices (device_name,last_heartbeat,has_gpu,return_message) VALUES
-	('takenaka_dl',current_timestamp,true,''),
-	('SMC101',current_timestamp,false,'Hello SMC!'),
-	('AGP092',current_timestamp,true,'Loooooooooooooong message!'),
-	('AGP093','2021-03-12 23:30:25',false,'');
-
-INSERT INTO public.gpu_machines (machine_id,last_detail) VALUES
-	(1,'testtesttest'),
-	(3,'Mon Mar  8 21:37:43 2021
+INSERT INTO public.devices (device_name,last_heartbeat,report,return_message) VALUES
+	('takenaka_dl',current_timestamp,'testtesttest',''),
+	('SMC101',current_timestamp,'','Hello SMC!'),
+	('AGP092',current_timestamp,'Mon Mar  8 21:37:43 2021
  -----------------------------------------------------------------------------
 | NVIDIA-SMI 450.102.04   Driver Version: 450.102.04   CUDA Version: 11.0     |
-|------------------------------- ---------------------- ---------------------- ');
+|------------------------------- ---------------------- ---------------------- ','Loooooooooooooong message!'),
+	('AGP093','2021-03-12 23:30:25','','');
 
 INSERT INTO public.jwt VALUES
 	('cc125635c56e2b29e842b7c520a5304eda31c3f0d409c09a911bcc5e742dcd60');
